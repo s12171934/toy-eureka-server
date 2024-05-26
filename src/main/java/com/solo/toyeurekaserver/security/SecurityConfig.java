66bb2e81cbeddff34ef21ec142ab27f1a6d1fafc
s12171934 <ssong99999@naver.com>
2024-05-26 12:42:41 +0900

    REFACTOR: build file, config import, application.properties to user config server

--- src/main/java/com/solo/toyeurekaserver/security/SecurityConfig.java
+++ src/main/java/com/solo/toyeurekaserver/security/SecurityConfig.java
@@ -1,13 +1,10 @@
 package com.solo.toyeurekaserver.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.*;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
